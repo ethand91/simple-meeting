dc3f9860a88db3c6e574b4cdb6670c75ed3da225
Ethan <edenvir99@gmail.com>
2022-07-31 15:15:33 +0900

    fix: static

--- src/express.js
+++ src/express.js
@@ -11,7 +11,7 @@ const roomRoutes = require('./routes/room.routes');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "app/build")));
+app.use(express.static(path.join(__dirname, "./../app/build")));
 
 app.use(bodyParser.json());
 app.use(compress());
@@ -19,6 +19,10 @@ app.use(helmet());
 app.use(cors());
 
 app.use('/', roomRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + './../app/build/index.html'));
+});
 
 const server = http.createServer(app);
 
